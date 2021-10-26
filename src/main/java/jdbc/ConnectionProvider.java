547f5ed2e80be6b3cacfe8905a2213d4253bbb40
Exequiel Farías <exequiel-farias@outlook.com>
2021-10-26 13:26:58 -0300

    se agrego el metodo para cerrar la conexion

--- src/main/java/jdbc/ConnectionProvider.java
+++ src/main/java/jdbc/ConnectionProvider.java
@@ -2,18 +2,29 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import excepciones.SQLExceptionCreated;
 
 public class ConnectionProvider {
 	
-	private static String url = "jdbc:sqlite:turismo-db.db";
+	private static String url = "jdbc:sqlite:turismo-tierra-media.db";
 	private static Connection connection;
 	
-	public static Connection getConnection() throws SQLException {
+	public static Connection getConnection(){
 		if(connection == null) {
-			connection = DriverManager.getConnection(url);
+			try {
+				connection = DriverManager.getConnection(url);
+			} catch (Exception e) {
+				throw new SQLExceptionCreated(e);
+			}
 		}
 		return connection;
 	}
 
+	public static void closeConnection() {
+		try {
+			connection.close();
+		} catch (Exception e) {
+			throw new SQLExceptionCreated(e);
+		}
+	}
 }
